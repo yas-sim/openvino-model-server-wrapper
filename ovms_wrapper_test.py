6d7d8fd9c983540274cf93f2310712a69758bdae
Yasunori Shimura <yasu0710@gmail.com>
2022-01-02 22:30:14 +0900

    update sample program

--- ovms_wrapper_test.py
+++ ovms_wrapper_test.py
@@ -5,14 +5,11 @@ import numpy as np
 
 ovms = OpenVINO_Model_Server()
 ovms.connect('127.0.0.1', 9000)
-status = ovms.get_model_status('resnet_50')
-print(status)
+#print(ovms.get_model_status('resnet_50'))
 model = ovms.open_model('resnet_50')
-
-image_file  = 'ovms/lib/python3.8/site-packages/skimage/data/rocket.jpg'
-
 print(model.inputs, model.outputs)
 
+image_file  = 'ovms/lib/python3.8/site-packages/skimage/data/rocket.jpg'
 img = cv2.imread(image_file)                # Read an image
 res = model.single_image_infer(img)         # Infer
 result = res[model.outputs[0]['name']]
