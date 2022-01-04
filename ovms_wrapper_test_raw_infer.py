1b7eb95b06ee88b989e3604806fac1bb0ae294d6
Yasunori Shimura <yasu0710@gmail.com>
2022-01-04 23:10:04 +0900

    add a sample code (raw_infer)

--- ovms_wrapper_test_raw_infer.py
+++ ovms_wrapper_test_raw_infer.py
@@ -8,11 +8,17 @@ ovms.connect('127.0.0.1', 9000)
 #print(ovms.get_model_status('resnet_50'))
 model = ovms.open_model('resnet_50')
 print(model.inputs, model.outputs)
+inblob = model.inputs[0]
 
 # curl -O https://raw.githubusercontent.com/intel-iot-devkit/smart-video-workshop/master/Labs/daisy.jpg
 image_file  = 'daisy.jpg'
-img = cv2.imread(image_file)                # Read an image
-res = model.single_image_infer(img)         # Single Image Infer
+img = cv2.imread(image_file)                    # Read an image
+img = cv2.resize(img, inblob['shape'][2:])
+img = img.transpose((2,0,1))
+img = img.reshape(inblob['shape']).astype(inblob['dtype_npy'])
+inblob_name = inblob['name']
+model.raw_infer({ inblob_name:img })         # Raw Infer
+res = model.parse_results()
 result = res[model.outputs[0]['name']]
 
 # display result
