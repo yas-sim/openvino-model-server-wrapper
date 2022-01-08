20030e6404077b47841729bf697d42e102af03b3
Yasunori Shimura <yasu0710@gmail.com>
2022-01-08 22:09:55 +0900

    separate OVMS_model_info class from OpenVINO_Model_Server class

--- ovms_wrapper/ovms_wrapper.py
+++ ovms_wrapper/ovms_wrapper.py
@@ -14,6 +14,81 @@ from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2_grpc, model_service_pb2_grpc
 from tensorflow_serving.apis import get_model_metadata_pb2, get_model_status_pb2
 
+class OVMS_model_info:
+    def __init__(self):
+        self.logger = logging.getLogger('OVMS_model')
+        sh = logging.StreamHandler(sys.stdout)
+        sh.setLevel(logging.INFO)
+        fmt = logging.Formatter("[%(levelname)s] %(name)s : %(message)s")
+        sh.setFormatter(fmt)
+        self.logger.addHandler(sh)
+
+        self.name = None
+        self.inputs = []
+        self.outputs = []
+        self.result = None
+        self.ovms = None
+        self.available = False
+
+    def search_blob(self, blobs, blob_name:str):
+        for idx, blob in enumerate(blobs):
+            if blob['name']==blob_name:
+                return idx
+        return -1
+
+    # inputs: Dictionary for input blobs
+    #   {'blobname1':contents, 'blobname2':contents, ...}
+    def raw_infer(self, inputs, timeout:float=10.0):
+        if self.available == False:
+            self.logger.error('Model "{}" is not available'.format(self.name))
+            return None
+        # create a request for inference
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = self.model_name
+        for bname, bval in inputs.items():
+            idx = self.search_blob(self.inputs, bname)
+            if idx == -1:
+                self.logger.error('Input blob "{}" is not found.'.format(bname))
+                return None
+            dim = [tensor_shape_pb2.TensorShapeProto.Dim(size=i) for i in bval.shape]
+            shape = tensor_shape_pb2.TensorShapeProto(dim=dim)
+            content = bval.tobytes()
+            dtype = self.inputs[idx]['dtype_pb2']
+            tensor = tensor_pb2.TensorProto(dtype=dtype, tensor_shape=shape, tensor_content=content)
+            request.inputs[bname].CopyFrom(tensor)
+        # submit infer request
+        self.result = self.ovms.stub.Predict(request, timeout) 
+        return self.result
+
+    # Parse and translate inference result to a dictionary style data
+    #   { 'outblob1':result, 'outblob2':result, ...}
+    def parse_results(self):
+        if self.available == False:
+            self.logger.error('Model "{}" is not available'.format(self.name))
+            return None
+        result = {}
+        for outblob in self.outputs:
+            tensor = self.result.outputs[outblob['name']]
+            shape = [dim.size for dim in tensor.tensor_shape.dim]
+            content = np.frombuffer(tensor.tensor_content, dtype=outblob['dtype_npy']).reshape(shape)
+            result[outblob['name']] = content
+        return result
+
+    def image_preprocess(self, blob, img:np.ndarray):
+        bdata = cv2.resize(img, tuple(blob['shape'][-1:-3:-1]))
+        bdata = bdata.transpose((2,0,1))
+        bdata = bdata.reshape(blob['shape']).astype(blob['dtype_npy'])
+        return bdata
+
+    def single_image_infer(self, img, timeout:float=10.0):
+        if self.available == False:
+            self.logger.error('Model "{}" is not available'.format(self.name))
+            return None
+        inblob = self.inputs[0]
+        bdata = self.image_preprocess(inblob, img)
+        self.raw_infer({inblob['name']:bdata})
+        return self.parse_results()
+
 class OpenVINO_Model_Server:
     state_names = { 0: "UNKNOWN", 10: "START", 20: "LOADING", 30: "AVAILABLE", 40: "UNLOADING", 50: "END" }
     dtype_str   = ['DT_INVALID', 'DT_FLOAT', 'DT_DOUBLE', 'DT_INT32', 'DT_UINT8', 'DT_INT16','DT_INT8', 
@@ -24,81 +99,6 @@ class OpenVINO_Model_Server:
                    np.unicode, np.complex64, np.int64, np.bool, np.int8, np.uint8, np.int32,
                    'DT_BFLOAT16', np.int16, np.uint16, np.uint16, np.complex128, np.float16, 
                    'DT_RESOURCE', 'DT_VARIANT', np.uint32, np.uint64]
-
-    class OVMS_model_info:
-        def __init__(self):
-            self.logger = logging.getLogger('OVMS_model')
-            sh = logging.StreamHandler(sys.stdout)
-            sh.setLevel(logging.INFO)
-            fmt = logging.Formatter("[%(levelname)s] %(name)s : %(message)s")
-            sh.setFormatter(fmt)
-            self.logger.addHandler(sh)
-
-            self.name = None
-            self.inputs = []
-            self.outputs = []
-            self.result = None
-            self.ovms = None
-            self.available = False
-
-        def search_blob(self, blobs, blob_name:str):
-            for idx, blob in enumerate(blobs):
-                if blob['name']==blob_name:
-                    return idx
-            return -1
-
-        # inputs: Dictionary for input blobs
-        #   {'blobname1':contents, 'blobname2':contents, ...}
-        def raw_infer(self, inputs, timeout:float=10.0):
-            if self.available == False:
-                self.logger.error('Model "{}" is not available'.format(self.name))
-                return None
-            # create a request for inference
-            request = predict_pb2.PredictRequest()
-            request.model_spec.name = self.model_name
-            for bname, bval in inputs.items():
-                idx = self.search_blob(self.inputs, bname)
-                if idx == -1:
-                    self.logger.error('Input blob "{}"not found.'.format(bname))
-                    return None
-                dim = [tensor_shape_pb2.TensorShapeProto.Dim(size=i) for i in bval.shape]
-                shape = tensor_shape_pb2.TensorShapeProto(dim=dim)
-                content = bval.tobytes()
-                dtype = self.inputs[idx]['dtype_pb2']
-                tensor = tensor_pb2.TensorProto(dtype=dtype, tensor_shape=shape, tensor_content=content)
-                request.inputs[bname].CopyFrom(tensor)
-            # submit infer request
-            self.result = self.ovms.stub.Predict(request, timeout) 
-            return self.result
-
-        # Parse and translate inference result to a dictionary style data
-        #   { 'outblob1':result, 'outblob2':result, ...}
-        def parse_results(self):
-            if self.available == False:
-                self.logger.error('Model "{}" is not available'.format(self.name))
-                return None
-            result = {}
-            for outblob in self.outputs:
-                tensor = self.result.outputs[outblob['name']]
-                shape = [dim.size for dim in tensor.tensor_shape.dim]
-                content = np.frombuffer(tensor.tensor_content, dtype=outblob['dtype_npy']).reshape(shape)
-                result[outblob['name']] = content
-            return result
-
-        def image_preprocess(self, blob, img:np.ndarray):
-            bdata = cv2.resize(img, tuple(blob['shape'][-1:-3:-1]))
-            bdata = bdata.transpose((2,0,1))
-            bdata = bdata.reshape(blob['shape']).astype(blob['dtype_npy'])
-            return bdata
-
-        def single_image_infer(self, img, timeout:float=10.0):
-            if self.available == False:
-                self.logger.error('Model "{}" is not available'.format(self.name))
-                return None
-            inblob = self.inputs[0]
-            bdata = self.image_preprocess(inblob, img)
-            self.raw_infer({inblob['name']:bdata})
-            return self.parse_results()
 
     def __init__(self):
         self.logger = logging.getLogger('OVMS_wrapper')
@@ -130,7 +130,7 @@ class OpenVINO_Model_Server:
         return status
 
     def open_model(self, model_name:str, timeout:float=10.0):
-        model = self.OVMS_model_info()
+        model = OVMS_model_info()
         model.model_name = model_name
         request = get_model_metadata_pb2.GetModelMetadataRequest()
         request.model_spec.name = model_name
@@ -148,21 +148,21 @@ class OpenVINO_Model_Server:
         signature_map = get_model_metadata_pb2.SignatureDefMap()
         signature_map.ParseFromString(signature_def.value)
         serving_default = signature_map.ListFields()[0][1]['serving_default']
-        self.serving_inputs = serving_default.inputs
-        self.serving_outputs = serving_default.outputs
+        serving_inputs = serving_default.inputs
+        serving_outputs = serving_default.outputs
         # parse input blob info
-        for key in self.serving_inputs.keys():
-            name = self.serving_inputs[key].name
-            shape = [dim.size for dim in self.serving_inputs[key].tensor_shape.dim]
-            dtype_pb2 = self.serving_inputs[key].dtype
+        for key in serving_inputs.keys():
+            name = serving_inputs[key].name
+            shape = [dim.size for dim in serving_inputs[key].tensor_shape.dim]
+            dtype_pb2 = serving_inputs[key].dtype
             dtype_npy = OpenVINO_Model_Server.dtype_npy[dtype_pb2]
             dtype_str = OpenVINO_Model_Server.dtype_str[dtype_pb2]
             model.inputs.append({'name':name, 'shape':shape, 'dtype_str':dtype_str, 'dtype_pb2':dtype_pb2, 'dtype_npy':dtype_npy})
         # parse output blob info
-        for key in self.serving_outputs.keys():
-            name = self.serving_outputs[key].name
-            shape = [dim.size for dim in self.serving_outputs[key].tensor_shape.dim]
-            dtype_pb2 = self.serving_outputs[key].dtype
+        for key in serving_outputs.keys():
+            name = serving_outputs[key].name
+            shape = [dim.size for dim in serving_outputs[key].tensor_shape.dim]
+            dtype_pb2 = serving_outputs[key].dtype
             dtype_npy = OpenVINO_Model_Server.dtype_npy[dtype_pb2]
             dtype_str = OpenVINO_Model_Server.dtype_str[dtype_pb2]
             model.outputs.append({'name':name, 'shape':shape, 'dtype_str':dtype_str, 'dtype_pb2':dtype_pb2, 'dtype_npy':dtype_npy})
